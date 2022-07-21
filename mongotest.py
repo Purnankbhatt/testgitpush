67225be052de117de2042b14f49272cca1ed7c7d
Purnank test <purnankbhatt4191@gmail.com>
2022-07-21 20:52:06 +0530

    this is my forst push code in GIT

---
+++ mongotest.py
@@ -0,0 +1,17 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://purnankbhatt:jaigurudatt@cluster0.hddmu.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name": "sainath",
+    "email":"purnankbhatt4191@gmail.com",
+    "surname":"datt"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
+
+
+# client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
